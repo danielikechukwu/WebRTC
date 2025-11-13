5bf0869ae2b16d4e68d565c5ac50eccc85974fee
Daniel ikechukwu <ikechukwudaniel080@gmail.com>
2025-11-13 16:48:19 +0000

    Refact(view-styling): implement new mobile view styling

--- src/app/room/room.ts
+++ src/app/room/room.ts
@@ -15,11 +15,9 @@ import { Router } from '@angular/router';
 })
 export default class Room implements OnInit {
   private signaling = inject(SignalingService);
-  private webrtc = inject(WebrtcService);
   private _toast = inject(HotToastService);
   private _router = inject(Router);
 
-  private mediaStream: MediaStream | null = null;
   private remoteStream: MediaStream | null = null;
   protected localStream: MediaStream | null = null;
 
@@ -54,9 +52,9 @@ export default class Room implements OnInit {
     this.checkPermissions(); // Check for media permission
 
     // this.signaling.connect('ws://192.168.0.3:8080');
-    this.signaling.connect('https://webrtc-signaling-server-o8h1.onrender.com');
+    // this.signaling.connect('https://webrtc-signaling-server-o8h1.onrender.com');
 
-    this.signaling.onMessage((data) => this.handleSignalingData(data));
+    // this.signaling.onMessage((data) => this.handleSignalingData(data));
   }
 
   async checkPermissions(): Promise<any> {
